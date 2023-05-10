98a5304d358db397ebac23a4fc554be337199da6
Mehtap <75047473+MehtapAyall@users.noreply.github.com>
2023-05-10 22:31:19 +0300

    haberler sayfası düzenlendi

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,5 +1,28 @@
-<ul>
-@foreach($news as $item)
-    <li><a href="{{ $item->link }}" target="_blank">{{ $item->title }}</a></li>
-@endforeach
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        @include('menu')
+    </div>
+
+    <div class="container ">
+        @foreach($news as $item)
+        <div class="news-item m-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $item->title }}</h5>
+
+                    <a href="{{ $item->link }}" class="btn btn-primary" target="_blank">Habere Git</a>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    </div>
+</body>
+</html>
