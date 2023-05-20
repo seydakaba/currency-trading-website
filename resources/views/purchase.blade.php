840691cf6fa95e77095c72cd283ef25621ba600f
Mehtap <75047473+MehtapAyall@users.noreply.github.com>
2023-05-20 15:05:02 +0300

    sayfa düzeni

--- resources/views/purchase.blade.php
+++ resources/views/purchase.blade.php
@@ -7,28 +7,44 @@
     <div>
         @include('menu')
     </div>
-    <h1>Döviz Satın Alma Formu</h1>
-    <form method="POST" action="{{ route('currency-purchase') }}" id="currency-purchase-form">
-        @csrf
-        <label for="account_id">Hesap Seçin:</label>
-        <select name="account_id" id="account_id">
-            @foreach($accounts as $account)
-                <option value="{{ $account->id }}">{{ $account->currency }} Hesabı (Bakiye: {{ $account->balance }})</option>
-            @endforeach
-        </select>
-        <br><br>
-        <label for="currency">Satın Alınacak Döviz:</label>
-        <select name="currency" id="currency">
-            @foreach($exchangeRates as $exchangeRate)
-                <option value="{{ $exchangeRate->currency }}">{{ $exchangeRate->currency }}</option>
-            @endforeach
-        </select>
-        <br><br>
-        <label for="amount">Miktar:</label>
-        <input type="number" step="0.01" name="amount" id="amount">
-        <br><br>
-        <button type="submit">Satın Al</button>
-    </form>
+    <div class="container mt-4">
+        <h1>Döviz Satın Alma Formu</h1>
+        <form method="POST" action="{{ route('currency-purchase') }}" id="currency-purchase-form">
+            @csrf
+            <table>
+                <tr>
+                    <td><label for="account_id">Hesap Seçin:</label></td>
+                    <td>
+                        <select name="account_id" id="account_id">
+                            @foreach($accounts as $account)
+                                <option value="{{ $account->id }}">{{ $account->currency }} Hesabı (Bakiye: {{ $account->balance }})</option>
+                            @endforeach
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="currency">Satın Alınacak Döviz:</label></td>
+                    <td>
+                        <select name="currency" id="currency">
+                            @foreach($exchangeRates as $exchangeRate)
+                                <option value="{{ $exchangeRate->currency }}">{{ $exchangeRate->currency }}</option>
+                            @endforeach
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="amount">Miktar:</label></td>
+                    <td><input type="number" step="0.01" name="amount" id="amount"></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <button type="submit" class="btn btn-primary">Satın Al</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>    
+
 </body>
 </html>
 
